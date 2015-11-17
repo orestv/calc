39bb8fe67502ea352299ce6f36133801ccfa8958
Orest Voloshchuk <orest.v@gmail.com>
2015-11-17 22:15:40 +0200

    WIP for material properties output.

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ import params
 
 class MaterialPicker(qw.QWidget):
     material_combobox = None
-    material_selected = qc.pyqtSignal(str, name='materialSelected')
+    material_selected = qc.pyqtSignal(params.MaterialProperties, name='materialSelected')
 
     def __init__(self):
         super(MaterialPicker, self).__init__()
-
         self.material_combobox = qw.QComboBox()
-
         self.init_ui()
 
     def init_ui(self):
@@ -30,32 +28,103 @@ class MaterialPicker(qw.QWidget):
         self.material_combobox.addItem(u'Мідь', params.MaterialProperties.COPPER)
         self.material_combobox.addItem(u'Алюміній', params.MaterialProperties.ALUMINIUM)
 
-        layout.addWidget(qw.QLabel("Матеріал"))
+        layout.addWidget(qw.QLabel(u'Матеріал'))
         layout.addWidget(self.material_combobox)
 
+    @property
+    def selected_material(self):
+        material_code = self.material_combobox.itemData(self.material_combobox.currentIndex())
+        material_properties = params.MaterialProperties(material_code)
+        return material_properties
+
     def _material_index_changed(self, material_index):
-        material_code = self.material_combobox.itemData(material_index)
-        self.material_selected.emit(material_code)
+        self.material_selected.emit(self.selected_material)
 
 
-class UI(qw.QWidget):
-    material_picker = None
+class MaterialProperty(qw.QWidget):
+    title = u''
+    fmt = '{:.3e}'
+    unit = u''
 
+    def __init__(self, title, fmt=None, unit=None):
+        super(MaterialProperty, self).__init__()
+
+        self.fmt = fmt or MaterialProperty.fmt
+        self.unit = unit or MaterialProperty.unit
+
+        self.lbl_title = qw.QLabel()
+        self.lbl_value = qw.QLabel()
+        self.lbl_unit = qw.QLabel()
+
+        self.lbl_title.setText(title)
+        self.lbl_unit.setText(unit)
+
+        layout = qw.QHBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(self.lbl_title)
+        layout.addStretch()
+        layout.addWidget(self.lbl_value)
+        layout.addWidget(self.lbl_unit)
+
+    def set_value(self, value):
+        s_value = self.fmt.format(value)
+        self.lbl_value.setText(s_value)
+
+
+class MaterialProperties(qw.QWidget):
     def __init__(self):
-        super(UI, self).__init__()
+        super(MaterialProperties, self).__init__()
 
-        self.material_picker = MaterialPicker()
+        self.sigma = MaterialProperty(u'К-т електропровідності σ', unit=u'1/Ом * м')
+        self.mu = MaterialProperty(u'Магнітна проникливість μ', unit=u'Гн/м')
+        self.nu = MaterialProperty(u'К-т Пуассона υ', fmt='{:.3f}')
 
         self.init_ui()
 
     def init_ui(self):
-        layout = qw.QGridLayout()
+        layout = qw.QVBoxLayout()
         self.setLayout(layout)
 
-        layout.addWidget(self.material_picker, 0, 0)
+        layout.addWidget(self.sigma)
+        layout.addWidget(self.mu)
+        layout.addWidget(self.nu)
 
-        self.setGeometry(300, 300, 200, 200)
-        self.setWindowTitle('Icon')
+    def update_widgets(self, material_properties):
+        print('Updating material properties to ' + material_properties.material_type)
+        self.sigma.set_value(material_properties['sigma'])
+        self.mu.set_value(material_properties['mu'])
+        self.nu.set_value(material_properties['nu'])
+
+    def set_material_properties(self, material_properties):
+        self.update_widgets(material_properties)
+
+
+class UI(qw.QWidget):
+    material_picker = None
+    material_properties = None
+    button_run = None
+
+    def __init__(self):
+        super(UI, self).__init__()
+        self.material_picker = MaterialPicker()
+        self.material_properties = MaterialProperties()
+        self.button_run = qw.QPushButton(text=u'Обчислити')
+
+        self.material_picker.material_selected.connect(self.material_properties.set_material_properties)
+        self.material_properties.set_material_properties(self.material_picker.selected_material)
+
+        self.init_ui()
+
+    def init_ui(self):
+        layout = qw.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.setWindowTitle(u'Параметри')
+
+        layout.addWidget(self.material_picker)
+        layout.addWidget(self.material_properties)
+        layout.addWidget(self.button_run)
 
 
 if __name__ == '__main__':
--- params.py
+++ params.py
@@ -1,4 +1,55 @@
+# coding = utf-8
+
+
 class MaterialProperties(object):
     STEEL = 'steel'
     COPPER = 'copper'
     ALUMINIUM = 'aluminium'
+
+    material_type = None
+    data = {
+        STEEL: {
+            'sigma': 0.135 * (10 ** 7),
+            'mu': 12.57 * (10 ** -7),
+            'nu': 0.283,
+            'k': 0.422 * (10 ** -5),
+            'lambda': 0.167 * (10 ** 2),
+            'E': 0.192 * (10 ** 12),
+            'alpha': 0.17 * (10 ** -4),
+            'rho': 7850,
+            'sigma_t': 300,
+        },
+        COPPER: {
+            'sigma': 0.588 * (10 ** 8),
+            'mu': 12.57 * (10 ** -7),
+            'nu': 0.35,
+            'k': 0.118 * (10 ** -3),
+            'lambda': 0.406 * (10 ** 3),
+            'E': 0.129 * (10 ** 12),
+            'alpha': 0.178 * (10 ** -4),
+            'rho': 78900,
+            'sigma_t': 70,
+        },
+        ALUMINIUM: {
+            'sigma': 0.363 * (10 ** 8),
+            'mu': 12.57 * (10 ** -7),
+            'nu': 0.34,
+            'k': 0.847 * (10 ** -4),
+            'lambda': 0.209 * (10 ** 3),
+            'E': 0.71 * (10 ** 11),
+            'alpha': 0.229 * (10 ** -4),
+            'rho': 2670,
+            'sigma_t': 30,
+        },
+    }
+
+    def __init__(self, material_type):
+        self.material_type = material_type
+
+    def __getitem__(self, item):
+        return self.data[self.material_type][item]
+
+
+
+class CalculationParameters(object):
+    impulse_duration = None
